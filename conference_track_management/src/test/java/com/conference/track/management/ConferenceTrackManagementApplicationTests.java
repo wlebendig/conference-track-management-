e287acbe0b2465a028eec85e0f7238b4c9fa1214
Werner Lebendig <werner@ramglobal.com>
2022-03-09 20:20:31 +0100

    Created new spring boot  project

---
+++ conference_track_management/src/test/java/com/conference/track/management/ConferenceTrackManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conference.track.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceTrackManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
